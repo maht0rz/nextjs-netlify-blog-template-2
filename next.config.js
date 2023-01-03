b59357e6ff9ad8bee8123b09b769c75363fe1f8f
Matej Sima <matej.sima@gmail.com>
2023-01-03 21:31:21 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    // config.module.rules.push({
+    //   test: /\.md$/,
+    //   loader: "remark-loader",
+    // });
+
+    return config
+  },
 }
 
 module.exports = nextConfig
